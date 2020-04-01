f051d86465821889a48ddb97625a2526df20300b
Daniel <daniel.slingerland@wur.nl>
2020-04-01 14:10:10 +0200

    Make

---
+++ cmake-build-debug/CMakeFiles/microtubule_polarity_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/microtubule_polarity_simulator.dir/main.cpp.obj"
+  "libmicrotubule_polarity_simulator.dll.a"
+  "microtubule_polarity_simulator.exe"
+  "microtubule_polarity_simulator.exe.manifest"
+  "microtubule_polarity_simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/microtubule_polarity_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

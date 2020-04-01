722c8c1a3f047dc64279509d1e6ed75f917caee7
Daniel <daniel.slingerland@wur.nl>
2020-04-01 17:03:13 +0200

    first day, final commit: - created class Microtubule - added variables: length, state - added functions: constructors, grow_shrink(), bind(), get functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 
+enum{GROW, SHRINK, BOUND, DEAD};
+const double GROWSPEED = 1;
+const double SHRINKSPEED = 5;
+
+class Microtubule{
+private:
+    double length;
+    int state;
+
+public:
+
+    Microtubule(){
+        length = 0.0;
+        state = GROW;
+
+    }
+    Microtubule(double l, int s){
+        length = l;
+        state = s;
+    };
+    void grow_shrink(){
+        if(state == GROW){
+            length += GROWSPEED;
+        } else if (state == SHRINK){
+            length -= SHRINKSPEED;
+            if(length <= 0){
+                state = DEAD;
+            }
+        }
+
+    }
+
+    void bind(bool binded){
+        if(binded) {
+            state = BOUND;
+        }else {
+            state = SHRINK;
+        }
+    }
+
+    double get_length(){
+        return length;
+    }
+
+    double get_state(){
+        return state;
+    }
+};
+
 int main() {
-    std::cout << "microtubles is commited";
+    std::cout << "microtubules is commited";
     return 0;
 }
+

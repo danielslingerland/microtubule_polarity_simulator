8b9f972598e8f3a94b28f26c94cf3b8ddcdcbe29
Daniel <daniel.slingerland@wur.nl>
2020-05-04 10:12:27 +0200

    GraphMaker.py adjusted to bin count

--- GraphMaker.py
+++ GraphMaker.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def read_data(filename):
+def read_data(filename, type_to):
+    # f = open("../DATA/" + filename, "r")
     f = open("../DATA/" + filename, "r")
     reading_data = False
     data = []
@@ -15,7 +16,7 @@ def read_data(filename):
         line = line.replace("\n", "")
         i += 1
         if reading_data:
-            data.append(float(line))
+            data.append(type_to(line))
         else:
             print(line)
             if line == "DATA":
@@ -54,7 +55,18 @@ def create_hist(data, title):
     plt.xlim(0, 1)
     plt.title(title)
     plt.grid()
-    plt.savefig("../DATA/MT_polarity" +str(i)+ ".png", bbox_inches='tight')
+    plt.savefig("../pictures/MT_polarity" +str(i)+ ".png", bbox_inches='tight')
+
+
+def create_bar(data, title):
+    x_as = []
+    for j in range(len(data)):
+        x_as.append(j/len(data))
+    plt.bar(data, x_as)
+    plt.title(title)
+    plt.xlabel("L/(L+R)")
+    plt.ylabel("counts")
+    plt.savefig("../pictures/MT_polarity" +str(i)+ ".png", bbox_inches='tight')
 
 
 def m0(v_grow, v_shrink, l_bar, growing):
@@ -65,51 +77,36 @@ def m0(v_grow, v_shrink, l_bar, growing):
     return 1 / (v_var * l_bar * ((1 / v_grow) + (1 / v_shrink)))
 
 
-def create_overview():
-    data = []
-    for i in range(1, 12):
-        data.append(read_data("MT_polarity"+str(i)+".txt"))
-    for d in data:
-        create_hist(d, "test")
+# def create_overview():
+#     data = []
+#     for i in range(1, 12):
+#         data.append(read_data("MT_polarity"+str(i)+".txt"))
+#     for d in data:
+#         create_hist(d, "test")
 
 
-#create_overview()
-#create_hist(read_data("MT_polarity10.txt"), "2.0e-7 per s per um ")
+def polarity_from_bins(data_bins):
+    denominator = 0
+    nominator = 0
+    for d in range(len(data_bins)):
+        denominator += data_bins[d]
+        polarity = abs((d/len(data_bins))-0.5+(1/len(data_bins))*0.5)
+        nominator += data_bins[d]*polarity
+    return nominator/denominator
+
 
 bindings = [0.00000000, 0.00000005, 0.00000008, 0.00000010, 0.00000011, 0.000000115, 0.00000012, 0.000000125, 0.00000013, 0.00000014, 0.00000015, 0.00000016, 0.00000017, 0.00000018, 0.00000019, 0.00000020, 0.00000021, 0.00000023, 0.00000025, 0.00000030]
 polarities = []
-for i in range(1,21):
-    data = read_data("MT_polarity"+str(i)+".txt")
-    create_hist(data, str(bindings[i-1])+" s-1 um-1")
-    data_man = [abs(j -0.5) for j in data]
-    polarities.append(sum(data_man)/len(data_man))
-
-
+for i in range(0, 20):
+    data = read_data("MT_polarity"+str(i)+".txt", int)
+    create_bar(data, str(bindings[i-1])+" s-1 um-1")
+    #data_man = [abs(j -0.5) for j in data]
+    polarities.append(polarity_from_bins(data))
+    plt.figure(i+2)
 
 plt.plot(bindings, polarities)
-plt.xlabel("bindings per second per micrometer (*10^-7)")
+plt.xlabel("binding rate (s^{-1} um^{-1})")
 plt.ylabel("average distance from 0.5")
-plt.savefig('../DATA/polarity_binding.png', bbox_inches='tight')
-#0.5e-7
-# testdata = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 140, 250, 300, 1, 2, 3, 4, 12, 4, 34, 23, 43]
-# m_test_null = m0(0.08, 0.16, 50, True)*len(testdata)
-# create_hist_fit(testdata, m_test_null, "test")
-# length = read_data("MT_length2.txt")
-# state = read_data("MT_state2.txt")
-#
-# growing_length = []
-#
-# shrinking_length = []
-#
-# for x in range(len(length)):
-#     if round(state[x]) == 0:
-#         growing_length.append(length[x])
-#     else:
-#         shrinking_length.append(length[x])
-#
-# print("finished splitting data in growing and shrinking")
-# m_plus_null = m0(0.08, 0.16, 50, True)*len(length)
-# m_minus_null = m0(0.08, 0.16, 50, False)*len(length)
-#
-# create_hist_fit(growing_length, m_plus_null, "Growing microtubules")
-# create_hist_fit(shrinking_length, m_minus_null, "Shrinking microtubules")
+plt.savefig("../pictures/overview.png", bbox_inches='tight')
+
+
